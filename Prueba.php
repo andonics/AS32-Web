6b41e273ae2f8944f79ae1916a957f0f4054f574
AS3-2-06 <AS3-2-06@uni.lan>
2018-09-20 13:52:49 +0200

    Horario de clase

--- Prueba.php
+++ Prueba.php
@@ -1,8 +1,100 @@
 <?php
-//strlen devuelve el numero de caractéres
-$str = 'abcdef';
-echo strlen($str); // 6
-echo "<br>";
-$str = ' ab cd ';
-echo strlen($str); // 7
+
+$hora = date("H");
+$dia = date("N");
+
+if ($dia = "1"){
+    if ($hora < "8") {
+      echo "No tienes clase";
+    }
+    elseif ($hora < "9") {
+      echo "BBDD";
+    }
+    elseif ($hora < "10") {
+      echo "EIE";
+    }
+    elseif ($hora < "12") {
+      echo "ASO";
+    }
+    elseif ($hora < "14") {
+      echo "IAW";
+    }
+    else {
+      echo "No tienes clase";
+    }
+}
+elseif ($dia = "2"){
+    if ($hora < "8") {
+      echo "No tienes clase";
+    }
+    elseif ($hora < "9") {
+      echo "IAW";
+    }
+    elseif ($hora < "10") {
+      echo "Seguridad";
+    }
+    elseif ($hora < "12") {
+      echo "SRI";
+    }
+    elseif ($hora < "14") {
+      echo "BBDD";
+    }
+    else {
+      echo "No tienes clase";
+    }
+}
+elseif ($dia = "3"){
+    if ($hora < "8") {
+      echo "No tienes clase";
+    }
+    elseif ($hora < "10") {
+      echo "Seguridad";
+    }
+    elseif ($hora < "12") {
+      echo "ASO";
+    }
+    elseif ($hora < "14") {
+      echo "EIE";
+    }
+    else {
+      echo "No tienes clase";
+    }
+}
+elseif ($dia = "4"){
+    if ($hora < "8") {
+      echo "No tienes clase";
+    }
+    elseif ($hora < "10") {
+      echo "SRI";
+    }
+    elseif ($hora < "12") {
+      echo "ASO";
+    }
+    elseif ($hora < "14") {
+      echo "IAW";
+    }
+    else {
+      echo "No tienes clase";
+    }
+}
+elseif ($dia = "5"){
+    if ($hora < "8") {
+      echo "No tienes clase";
+    }
+    elseif ($hora < "10") {
+      echo "Seguridad";
+    }
+    elseif ($hora < "12") {
+      echo "SRI";
+    }
+    elseif ($hora < "14") {
+      echo "Ingles";
+    }
+    else {
+      echo "No tienes clase";
+    }
+}
+else {
+  echo "No tienes clase";
+}
 ?>
