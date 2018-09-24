68621cf598712b86149fc7eb6ab9ff9c487ad5bf
AS3-2-06 <AS3-2-06@uni.lan>
2018-09-24 13:20:09 +0200

    Array + Foreach

--- array.php
+++ array.php
@@ -4,7 +4,13 @@ $cars = array("Volvo", "BMW", "Toyota");
 //echo "I like " . $cars[2] . ", " . $cars[0] . " and " . $cars[1] . ".";
 //echo count($cars)
 
-for ($x = 0; $x < count($cars); $x++) {
+/*for ($x = 0; $x < count($cars); $x++) {
   echo $cars[$x];
   echo '<br>';
+}
+*/
+
+foreach($cars as $coche) {
+  echo $coche;
+  echo "<br>";
 }
