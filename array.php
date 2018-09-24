bb419f75c378008bccd61f764b4664030dc9e112
AS3-2-06 <AS3-2-06@uni.lan>
2018-09-24 13:15:30 +0200

    array + for

--- array.php
+++ array.php
@@ -1,3 +1,10 @@
 <?php
+
 $cars = array("Volvo", "BMW", "Toyota");
-echo "I like " . $cars[2] . ", " . $cars[0] . " and " . $cars[1] . ".";
+//echo "I like " . $cars[2] . ", " . $cars[0] . " and " . $cars[1] . ".";
+//echo count($cars)
+
+for ($x = 0; $x < count($cars); $x++) {
+  echo $cars[$x];
+  echo '<br>';
+}
