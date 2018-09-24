6983c90a44568990f00aa745ec9ae18fc1970e20
AS3-2-06 <AS3-2-06@uni.lan>
2018-09-24 13:26:43 +0200

    Associative Array

--- array.php
+++ array.php
@@ -1,16 +1,26 @@
 <?php
 
-$cars = array("Volvo", "BMW", "Toyota");
+//$cars = array("Volvo", "BMW", "Toyota");
 //echo "I like " . $cars[2] . ", " . $cars[0] . " and " . $cars[1] . ".";
 //echo count($cars)
 
-/*for ($x = 0; $x < count($cars); $x++) {
+/* ----------------FOR----------------
+
+for ($x = 0; $x < count($cars); $x++) {
   echo $cars[$x];
   echo '<br>';
 }
+--------------------------------------
 */
+
+/* -------------FOREACH---------------
 
 foreach($cars as $coche) {
   echo $coche;
   echo "<br>";
 }
+--------------------------------------
+*/
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+echo $age['Peter']; //$age[0]
