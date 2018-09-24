4c11fbada815e9658d02222876381693e78ea85b
AS3-2-06 <AS3-2-06@uni.lan>
2018-09-24 12:54:49 +0200

    Loops

--- loops.php
+++ loops.php
@@ -4,6 +4,16 @@
 
 // print 'a' 100 veces
 
+//WHILE
+
+$x = 0;
+while($x < 100) {
+    echo "The number is: $x <br>";
+    $x++;
+}
+
+// FOR
+
 for ($x = 1; $x <= 100; $x++) {
   echo 'vuelta '. $x . ': ';
   echo 'a';
