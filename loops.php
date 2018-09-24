04eea18cc44150d664590ac14d1fa2d7f9e425c8
AS3-2-06 <AS3-2-06@uni.lan>
2018-09-24 12:52:42 +0200

    For and While

---
+++ loops.php
@@ -0,0 +1,11 @@
+<?php
+
+// https://www.w3schools.com/php/php_looping_for.asp
+
+// print 'a' 100 veces
+
+for ($x = 1; $x <= 100; $x++) {
+  echo 'vuelta '. $x . ': ';
+  echo 'a';
+  echo '<br>';
+}
