3eb59a066142c8476b0da87a299614d2e9eae094
AS3-2-06 <AS3-2-06@uni.lan>
2018-10-04 13:36:50 +0200

    Login_DB

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+
+// Read from the formulary (login.html)
+// user / Password
+echo $_POST['usuario'];
+echo '<br>';
+echo $_POST['password'];
+//connect to mysql
+
+// select from users where
+
+//if ok then go to loginok.html
+
+//if not ok return to login.html
