5037f182d0964e12de05e64e06d67c9f5f11a7dc
AS3-2-06 <AS3-2-06@uni.lan>
2018-10-08 13:33:30 +0200

    Login ok

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 // Read from the formulary (login.html)
 // user / Password
 $user = $_POST['user'];
-$password = $_POST['password'];
+$passwd = $_POST['passwd'];
 
 //connect to mysql
 $servername = 'localhost:3307';
@@ -16,9 +16,12 @@ $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 
 // select from users where
+$query = "SELECT * FROM users WHERE user='$user' AND password='$passwd'";
+echo  $query;
+
 
 //if ok then go to loginok.html
 
